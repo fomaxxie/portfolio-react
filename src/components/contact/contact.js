67299839fc221f140b0f38646cba4d9177cf0284
Maksim Fomkin <fomaxxi@gmail.com>
2023-10-31 21:47:38 +0100

    adding contact form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useRef } from 'react';
 import "./contact.css";
+import emailjs from '@emailjs/browser';
 
-const Contact = () => {
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_b4z8xon', 'template_s9vkip6', form.current, 'iQkZfO9pZG0s4ehA5')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
   return (
     <section id="contact">
       <h2>Get In Touch</h2>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </section>
-  )
-}
+  );
+};
 
 export default Contact
