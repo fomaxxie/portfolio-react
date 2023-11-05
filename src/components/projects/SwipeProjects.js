7024ecc90bf2fb5e9e4ad82ee8f1dad8cb1aa6ae
Maksim Fomkin <fomaxxi@gmail.com>
2023-11-05 17:33:18 +0100

    swiper integrated

---
+++ src/components/projects/SwipeProjects.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './SwipeProjects.css';
+
+const SwipeProjects = ({ imgs }) => {
+  return (
+    <Swiper
+      modules={[Navigation]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+    >
+      {imgs.map((img, index) => (
+        <SwiperSlide key={index}>
+          <img src={img} alt={`Slide ${index}`} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default SwipeProjects;
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -4,27 +4,31 @@ import IMG1_1 from "../../assets/projects/midpoint-1.png";
 import IMG1_2 from "../../assets/projects/midpoint-2.png";
 import IMG1_3 from "../../assets/projects/midpoint-3.png";
 import IMG2_1 from "../../assets/projects/spaces-1.png";
+import IMG2_2 from "../../assets/projects/spaces-2.png";
+import IMG2_3 from "../../assets/projects/spaces-3.png";
 import IMG3_1 from "../../assets/projects/prcrft.png";
-import SwiperProjects from "./swiper";
+import IMG3_2 from "../../assets/projects/prcrft-2.png";
+import IMG3_3 from "../../assets/projects/prcrft-3.png";
+import SwipeProjects from "./SwipeProjects";
 
 const data = [
   {
     id: 1,
-    images: [IMG1_1, IMG1_2, IMG1_3],
+    imgs: [IMG1_1, IMG1_2, IMG1_3],
     title: "Midpoint",
     github: "https://github.com/fomaxxie/midpoint",
     link: "https://www.midpoint.world/",
   },
   {
     id: 2,
-    images: [IMG2_1, IMG1_2, IMG1_3],
+    imgs: [IMG2_1, IMG2_2, IMG2_3],
     title: "Spaces",
     github: "https://github.com/fomaxxie/spaces",
     link: "https://spaces-dgraham09-0d4ed9f6ae40.herokuapp.com/",
   },
   {
     id: 3,
-    images: [IMG3_1, IMG1_2, IMG1_3],
+    imgs: [IMG3_1, IMG3_2, IMG3_3],
     title: "Pristine Crafts (ecom store)",
     github: " link",
     link: "link",
@@ -37,11 +41,11 @@ function Projects() {
       <div className = "projects-container">
         <h2 style={{textAlign: "center"}}>Projects</h2>
         <div className = "projects-cards">
-          {data.map(({id, images, title, github, link}) => {
+          {data.map(({id, imgs, title, github, link}) => {
             return (
               <article key={id} >
                 <div className = "project-card">
-                  <SwiperProjects images={images} />
+                  <SwipeProjects imgs={imgs}/>
                   <div className = "project-description">
                     <h3>{title}</h3>
                     <div className="project-cta">
