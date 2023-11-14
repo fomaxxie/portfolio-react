28aefb19f6e4a1a03575865cc0081757608d9b9f
Maksim Fomkin <fomaxxi@gmail.com>
2023-11-14 21:55:05 +0100

    contact form fix - message

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,18 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import "./contact.css";
 import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
   const form = useRef();
+  const [sending, setSending] = useState(false);
+  const [message, setMessage] = useState('');
 
   const sendEmail = (e) => {
     e.preventDefault();
+    setSending(true);
+    setMessage('');
 
     emailjs.sendForm('service_b4z8xon', 'template_abeti4r', form.current, 'iQkZfO9pZG0s4ehA5')
       .then((result) => {
           console.log(result.text);
+          setMessage('Message sent successfully!');
+          setSending(false);
       }, (error) => {
           console.log(error.text);
+          setMessage('Failed to send message. Please try again later.');
+          setSending(false);
       });
   };
 
@@ -24,7 +32,10 @@ export const Contact = () => {
           <input type="text" name='user_name' placeholder='Your Name' required />
           <input type="email" name="user_email" placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required />
-          <button type="submit" className='btn btn-contact' value="Send">Send Message</button>
+          <button type="submit" className='btn btn-contact' disabled={sending}>
+            {sending ? 'Sending...' : 'Send Message'}
+          </button>
+           {message && <p>{message}</p>}
         </form>
       </div>
     </section>
